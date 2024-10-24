efd33c8a8816cd5c2245792d27c3e9e28c5ef7a2
xmin <z_xmin@foxmail.com>
2024-10-24 11:34:09 +0800

    ✨ feat(task/mod.rs): add some fields
    add some fields on TaskControlBlock for task_info

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,5 +1,7 @@
 //! Types related to task management
 
+use crate::config::MAX_SYSCALL_NUM;
+
 use super::TaskContext;
 
 /// The task control block (TCB) of a task.
@@ -9,6 +11,10 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// The time when the first schedule occurs
+    pub first_schedule_time: usize,
+    /// Array to count the number of each type of syscall
+    pub syscall_count: [u32; MAX_SYSCALL_NUM],
 }
 
 /// The status of a task
